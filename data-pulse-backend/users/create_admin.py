24824624b22c38642ca47b665914337f827aa83c
DavidRodriguez <david.rodriguez@transmilenio.gov.co>
2026-03-06 10:26:14 -0500

    adaptacion para crear usuario admin en render por consola

--- data-pulse-backend/users/create_admin.py
+++ data-pulse-backend/users/create_admin.py
@@ -1,13 +1,23 @@
 import os
 import django
 
+# Configura el entorno de Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 django.setup()
 
 from users.models import User
 
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'david.rodriguez@correo.com', 'demo1234')
+# Usamos 'email' porque es tu USERNAME_FIELD
+email_admin = 'admin@datapulse.com'
+
+if not User.objects.filter(email=email_admin).exists():
+    # Creamos el superusuario usando los campos que definiste en tu modelo
+    # 'nombre_completo' es requerido en tu JSON de Postman, así que lo incluimos
+    User.objects.create_superuser(
+        email=email_admin, 
+        nombre_completo='Admin DataPulse', 
+        password='Password123*'
+    )
     print("Superusuario creado con éxito")
 else:
-    print("El usuario ya existe")
+    print("El usuario admin ya existe")
