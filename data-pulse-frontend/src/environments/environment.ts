512457be6c3dcc245e72167f47174346e56dca84
DavidRodriguez <david.rodriguez@transmilenio.gov.co>
2026-03-06 12:36:21 -0500

    usar url de produccion

--- data-pulse-frontend/src/environments/environment.ts
+++ data-pulse-frontend/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8000/api' // URL base de tu backend en Django
+    //production: false,
+    //apiUrl: 'http://localhost:8000/api' // URL base de tu backend en Django
+    production: true, // Cámbialo a true si es el archivo principal
+    apiUrl: 'https://datapulse-ud95.onrender.com/api'
 };
